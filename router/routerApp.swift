29d84a0facfc469c8d398684eb331d5583b6c863
dong nguyen <dongka95@gmail.com>
2024-08-26 16:45:05 +0700

    add background

--- router/routerApp.swift
+++ router/routerApp.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 @main
 struct routerApp: App {
-    
     @ObservedObject private var router = DNRouter()
     
     var body: some Scene {
@@ -31,7 +30,6 @@ struct routerApp: App {
 }
 
 struct HomeView: View {
-
     @EnvironmentObject var router: DNRouter
 
     var body: some View {
@@ -42,87 +40,83 @@ struct HomeView: View {
             Text("**Home**")
                 .font(.system(size: 24))
             
-            
             Button(action: {
                 router.navigate(to: .livingroom)
             }, label: {
                 Text("**Go to Livingroom**")
             })
             .padding(12)
-            
-
         }
         .padding()
     }
 }
 
-
 struct LivingroomView: View {
-    
     @EnvironmentObject var router: DNRouter
     
     var body: some View {
-        VStack {
-            Image(systemName: "sofa.fill")
-                .font(.system(size: 56))
-                .foregroundColor(.accentColor)
-            Text("**Livingroom**")
-                .font(.system(size: 24))
+        ZStack {
+            Color.green.ignoresSafeArea()
+            VStack {
+                Image(systemName: "sofa.fill")
+                    .font(.system(size: 56))
+                    .foregroundColor(.accentColor)
+                Text("**Livingroom**")
+                    .font(.system(size: 24))
+                    .padding(.top, 12)
+                
+                Button("**Go to Duyen's Bedroom**") {
+                    router.navigate(to: .bedroom(owner: "Jane"))
+                }
                 .padding(.top, 12)
-            
-            Button("**Go to Duyen's Bedroom**") {
-                router.navigate(to: .bedroom(owner: "Jane"))
+                
+                Button("**Go to Dong's Bedroom**") {
+                    router.navigate(to: .bedroom(owner: "John"))
+                }
+                .padding(.top, 12)
+                
+                Button {
+                    router.navigateBack()
+                } label: {
+                    Text("Back")
+                }
+                .padding(.top, 4)
             }
-            .padding(.top, 12)
-            
-            Button("**Go to Dong's Bedroom**") {
-                router.navigate(to: .bedroom(owner: "John"))
-            }
-            .padding(.top, 12)
-            
-            Button {
-                router.navigateBack()
-            } label: {
-                Text("Back")
-            }
-            .padding(.top, 4)
         }
         .navigationBarBackButtonHidden()
-        .padding()
     }
 }
 
-
 struct BedroomView: View {
-    
     @EnvironmentObject var router: DNRouter
     var ownerName: String
     
     var body: some View {
-        VStack {
-            Text("\(ownerName)'s Bedroom")
-                .font(.system(size: 36, weight: .bold))
-                .padding(.bottom, 12)
-            Image(systemName: "bed.double.fill")
-                .font(.system(size: 56))
-                .foregroundColor(.accentColor)
-            
-            Button {
-                router.navigateBack()
-            } label: {
-                Text("Back to **Livingroom**")
+        ZStack {
+            Color.red.ignoresSafeArea()
+            VStack {
+                Text("\(ownerName)'s Bedroom")
+                    .font(.system(size: 36, weight: .bold))
+                    .padding(.bottom, 12)
+                Image(systemName: "bed.double.fill")
+                    .font(.system(size: 56))
+                    .foregroundColor(.accentColor)
+                
+                Button {
+                    router.navigateBack()
+                } label: {
+                    Text("Back to **Livingroom**")
+                }
+                .padding(.top, 12)
+                
+                Button {
+                    router.navigateToRoot()
+                } label: {
+                    Text("Pop to **Home**")
+                }
+                .padding(.top, 4)
             }
-            .padding(.top, 12)
-            
-            Button {
-                router.navigateToRoot()
-            } label: {
-                Text("Pop to **Home**")
-            }
-            .padding(.top, 4)
-
         }
         .navigationBarBackButtonHidden()
-        .padding()
     }
 }
